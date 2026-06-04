4c1212717d41fe49f53baa9f9a5f2553df57e6fe
Seunghwan Ji <hianono439@gmail.com>
2026-06-04 20:13:35 +0900

    report_과제제출_지승환

--- library/models/book.py
+++ library/models/book.py
@@ -18,17 +18,22 @@ class Book:
     year: int
 
     # TODO: 클래스 변수 book_count 선언 및 증가 로직 추가
+    book_count = 0
     # TODO: 생성 시 book_count 증가
     # book_count: int = 0  # 힌트: dataclass의 필드가 아닌 클래스 속성으로 선언
-    def __init__(self):
-        pass
+    def __init__(self,title, author, year):
+        self.title = title
+        self.author =  author
+        self.year = year
+        Book.book_count += 1
+
 
     def __str__(self) -> str:
         # TODO: 지정된 문자열 포맷 반환
         # e.g. 책이름 by 지은이 (2001)
-        return None
+        return f"{self.title} by {self.author} ({self.year})"
 
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> "Book":
         # TODO: dict에서 title/author/year를 읽어 Book 생성
-        raise NotImplementedError
+        return cls(title=data.get("title"), author=data.get("author"), year=data.get("year"))
